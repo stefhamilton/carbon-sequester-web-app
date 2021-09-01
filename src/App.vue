27478b35f94a69211f79746999509ab18cd3af31
Jared Potter <jaredpotter1@gmail.com>
2021-08-31 22:37:32 -0600

    Add github workflows and scss

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-container">
+    Hello Stefan
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
 
-<style>
-#app {
+<style lang="scss">
+.app-container {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
